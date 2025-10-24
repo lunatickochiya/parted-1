6430c897363929dd2cf962074e9fe337aee8f100
Brian C. Lane <bcl@redhat.com>
2025-10-24 16:57:56 -0700

    tests: Use license URL not the old address

--- tests/t-lvm.sh
+++ tests/t-lvm.sh
@@ -8,8 +8,7 @@
 # of the GNU General Public License v.2.
 #
 # You should have received a copy of the GNU General Public License
-# along with this program; if not, write to the Free Software Foundation,
-# Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+# along with this program; if not see https://www.gnu.org/licenses/
 
 export LVM_SUPPRESS_FD_WARNINGS=1
 
